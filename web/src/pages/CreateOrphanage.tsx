54b771ab4d03649ab235509f33e6499ed5ec871b
lucasantos603 <lucasantos603@gmail.com>
2020-10-15 16:28:49 -0300

    NLW 3 aula 3 Parte 3 (sem bugs)

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,50 +1,31 @@
 import React from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
-
-import mapMarkerImg from '../images/map-marker.svg';
+import { FiPlus } from "react-icons/fi";
 
 import '../styles/pages/create-orphanage.css';
-
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+import Sidebar from "../components/sidebar";
+import mapIcon from "../util/mapIcon";
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
 
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
+            <Map
+              center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
             >
               <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker interactive={false} icon={mapIcon} position={[-27.2092052, -49.6401092]} />
             </Map>
 
             <div className="input-block">
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import { Link } from "react-router-dom"
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import Leaflet from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
 import mapMakingImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMakingImg,
-
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+import mapIcon from '../util/mapIcon';
 
 function OrphanegesMap() {
     return (
